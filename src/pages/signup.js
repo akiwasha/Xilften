f9af065e0278e55c91b3194a5d39a7450dcf39d0
Pierre-Laurent VINCENT <plvincent.pro@gmail.com>
2021-01-16 19:14:44 +0100

    added signup page and auth listener hook

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -1,5 +1,85 @@
-import React from 'react';
+import React, { useState, useContext } from 'react';
+import { useHistory } from 'react-router-dom';
+import { FirebaseContext } from '../context/firebase';
+import { FooterContainer } from '../containers/footer';
+import { HeaderContainer } from '../containers/header';
+import { Form } from '../components';
+import * as ROUTES from '../constants/routes';
 
 export default function Signup() {
-  return <p>Hello from the sign up!</p>;
+  const history = useHistory();
+  const { firebase } = useContext(FirebaseContext);
+
+  const [firstName, setFirstName] = useState('');
+  const [emailAddress, setEmailAddress] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+
+  const isInvalid = firstName === '' || password === '' || emailAddress === '';
+
+  const handleSignUp = (event) => {
+    event.preventDefault();
+
+    firebase
+      .auth()
+      .createUserWithEmailAndPassword(emailAddress, password)
+      .then((result) =>
+        result.user
+          .updateProfile({
+            displayName: firstName,
+            photoURL: Math.floor(Math.random() * 5) + 1,
+          })
+          .then(() => {
+            history.push(ROUTES.BROWSE);
+          })
+      )
+      .catch((error) => {
+        setFirstName('');
+        setEmailAddress('');
+        setPassword('');
+        setError(error.message);
+      });
+  };
+  return (
+    <>
+      <HeaderContainer>
+        <Form>
+          <Form.Title>S'inscrire</Form.Title>
+          {error && <Form.Error>{error}</Form.Error>}
+
+          <Form.Base onSubmit={handleSignUp} method="POST">
+            <Form.Input
+              placeholder="Prénom"
+              value={firstName}
+              onChange={({ target }) => setFirstName(target.value)}
+            />
+            <Form.Input
+              placeholder="Adresse email"
+              value={emailAddress}
+              onChange={({ target }) => setEmailAddress(target.value)}
+            />
+            <Form.Input
+              type="password"
+              value={password}
+              autocomplete="off"
+              placeholder="Mot de passe"
+              onChange={({ target }) => setPassword(target.value)}
+            />
+            <Form.Submit disabled={isInvalid} type="submit">
+              Continuer
+            </Form.Submit>
+            <Form.Text>
+              Déjà utilisateur de Netflix?
+              <Form.Link to="/signin">S'identifier</Form.Link>
+            </Form.Text>
+            <Form.TextSmall>
+              Cette page est protégée par Google reCAPTCHA pour nous assurer que
+              vous n'êtes pas un robot.
+            </Form.TextSmall>
+          </Form.Base>
+        </Form>
+      </HeaderContainer>
+      <FooterContainer />
+    </>
+  );
 }
